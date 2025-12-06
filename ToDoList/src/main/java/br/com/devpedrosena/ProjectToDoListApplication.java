c75352f3654769b17bbdca13648f0e45a6ce5444
devpedrosena1 <devpedrosena1@gmail.com>
2025-12-06 00:20:51 -0300

    creating project spring and creating class tarefa

---
+++ ToDoList/src/main/java/br/com/devpedrosena/ProjectToDoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.devpedrosena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToDoListApplication.class, args);
+	}
+
+}
---
+++ ToDoList/src/test/java/br/com/devpedrosena/ProjectToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devpedrosena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
